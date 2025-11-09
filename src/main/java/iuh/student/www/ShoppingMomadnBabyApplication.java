62a3852c0257d9f840bb610bfc84e96372ab29be
vicute0707 <vi0978294041@gmail.com>
2025-11-09 20:40:30 +0700

    1th commit

---
+++ src/main/java/iuh/student/www/ShoppingMomadnBabyApplication.java
@@ -0,0 +1,13 @@
+package iuh.student.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMomadnBabyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMomadnBabyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/student/www/ShoppingMomadnBabyApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.student.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMomadnBabyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
